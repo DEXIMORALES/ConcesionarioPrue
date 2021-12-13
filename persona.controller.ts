e9708667420ec603c71e4c569709bf2985a0f886
DEXIMORALES <90730653+DEXIMORALES@users.noreply.github.com>
2021-12-13 13:16:08 -0500

    Add files via upload

---
+++ persona.controller.ts
@@ -0,0 +1,195 @@
+import {service} from '@loopback/core';
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where
+} from '@loopback/repository';
+import {
+  del, get,
+  getModelSchemaRef, HttpErrors, param, patch, post, put, requestBody,
+  response
+} from '@loopback/rest';
+import {Llaves} from "../config/Llaves";
+import {Credenciales, Persona} from '../models';
+import {PersonaRepository} from '../repositories';
+import {AutenticacionService} from '../services';
+const fetch = require("node-fetch");
+
+export class PersonaController {
+  constructor(
+    @repository(PersonaRepository)
+    public personaRepository: PersonaRepository,
+    @service(AutenticacionService)
+    public servicioAutenticacion: AutenticacionService,
+  ) { }
+
+  @post("/identificarPersona", {
+    responses: {
+      "200": {
+        description: "identificación de usuarios"
+      }
+    }
+  })
+  async identificarPersona(
+    @requestBody() credenciales: Credenciales) {
+    let p = await this.servicioAutenticacion.IdentificarPersona(credenciales.usuario, credenciales.clave);
+    if (p) {
+      let token = this.servicioAutenticacion.GenerarTokenJWT(p);
+      return {
+        datos: {
+          nombre: p.Nombre,
+          correo: p.correo,
+          id: p.id
+        },
+        tk: token
+      }
+    } else {
+      throw new HttpErrors[401]("Los datos suministrados no son válidos");
+    }
+  }
+
+
+  @post('/personas')
+  @response(200, {
+    description: 'Persona model instance',
+    content: {'application/json': {schema: getModelSchemaRef(Persona)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Persona, {
+            title: 'NewPersona',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    persona: Omit<Persona, 'id'>,
+  ): Promise<Persona> {
+
+    let clave = this.servicioAutenticacion.GenerarClave();
+    let claveCifrada = this.servicioAutenticacion.CifrarClave(clave);
+    persona.Clave = claveCifrada;
+
+
+    let p = await this.personaRepository.create(persona);
+
+    //Notificacion al usuario
+    let destino = persona.correo;
+    let asunto = "Registro en la app Pedido";
+    let contenido = `Hola, ${persona.Nombre}, su nombre de usuario es: ${persona.correo}, y la contraseña para el acceso a la app es:${clave}`;
+
+    fetch(`http://localhost:/envio-correo?correo_destino=${destino}&asunto=${asunto}&contenido=${contenido}`)
+      .then((data: any) => {
+        console.log(data);
+      });
+
+    return p;
+  }
+
+
+  @get('/personas/count')
+  @response(200, {
+    description: 'Persona model count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async count(
+    @param.where(Persona) where?: Where<Persona>,
+  ): Promise<Count> {
+    return this.personaRepository.count(where);
+  }
+
+  @get('/personas')
+  @response(200, {
+    description: 'Array of Persona model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Persona, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(Persona) filter?: Filter<Persona>,
+  ): Promise<Persona[]> {
+    return this.personaRepository.find(filter);
+  }
+
+  @patch('/personas')
+  @response(200, {
+    description: 'Persona PATCH success count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Persona, {partial: true}),
+        },
+      },
+    })
+    persona: Persona,
+    @param.where(Persona) where?: Where<Persona>,
+  ): Promise<Count> {
+    return this.personaRepository.updateAll(persona, where);
+  }
+
+  @get('/personas/{id}')
+  @response(200, {
+    description: 'Persona model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(Persona, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.string('id') id: string,
+    @param.filter(Persona, {exclude: 'where'}) filter?: FilterExcludingWhere<Persona>
+  ): Promise<Persona> {
+    return this.personaRepository.findById(id, filter);
+  }
+
+  @patch('/personas/{id}')
+  @response(204, {
+    description: 'Persona PATCH success',
+  })
+  async updateById(
+    @param.path.string('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Persona, {partial: true}),
+        },
+      },
+    })
+    persona: Persona,
+  ): Promise<void> {
+    await this.personaRepository.updateById(id, persona);
+  }
+
+  @put('/personas/{id}')
+  @response(204, {
+    description: 'Persona PUT success',
+  })
+  async replaceById(
+    @param.path.string('id') id: string,
+    @requestBody() persona: Persona,
+  ): Promise<void> {
+    await this.personaRepository.replaceById(id, persona);
+  }
+
+  @del('/personas/{id}')
+  @response(204, {
+    description: 'Persona DELETE success',
+  })
+  async deleteById(@param.path.string('id') id: string): Promise<void> {
+    await this.personaRepository.deleteById(id);
+  }
+}
---
+++ persona.model.ts
@@ -0,0 +1,55 @@
+import {Entity, hasMany, model, property} from '@loopback/repository';
+import {Pedidos} from './pedidos.model';
+
+@model()
+export class Persona extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Apellidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @property({
+    type: 'string',
+    required: false,
+  })
+  Clave: string;
+
+  @hasMany(() => Pedidos)
+  pedidos: Pedidos[];
+
+  constructor(data?: Partial<Persona>) {
+    super(data);
+  }
+}
+
+export interface PersonaRelations {
+  // describe navigational properties here
+}
+
+export type PersonaWithRelations = Persona & PersonaRelations;
